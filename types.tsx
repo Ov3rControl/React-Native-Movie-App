58c6e1ec0984a0f4f1f1580906cb494b1643db09
Mohamed Adel Ibrahim <ibrahim@riseup.co>
2020-08-01 18:28:43 +0200

    :sparkles: Types

--- types.tsx
+++ types.tsx
@@ -6,6 +6,7 @@ export type RootStackParamList = {
 export type BottomTabParamList = {
   Home: undefined;
   Search: undefined;
+  Cart: undefined;
 };
 
 export type TabOneParamList = {
@@ -14,4 +15,7 @@ export type TabOneParamList = {
 
 export type TabTwoParamList = {
   SearchScreen: undefined;
+};
+export type TabThreeParamList = {
+  CartScreen: undefined;
 };
